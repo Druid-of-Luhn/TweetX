c946c635afcfb9669901a7da24f1c1bf167cf5a2
db213 <db213@pc3-026-l.cs.st-andrews.ac.uk>
2016-10-08 18:28:45 +0100

    increased force

--- tweetx/entity.py
+++ tweetx/entity.py
@@ -37,7 +37,7 @@ class Spaceship(Entity):
     WIDTH = 1
     HEIGHT = 1
     TURN_RADIANS = 0.174533
-    FORCE = 1
+    FORCE = 5
 
     def __init__(self, x, y):
         super().__init__('spaceship', x, y, self.WIDTH, self.HEIGHT)
@@ -45,7 +45,7 @@ class Spaceship(Entity):
         self.direction_orientation = self.direction_velocity
         self.ammo = 15
         self.health = 5
-        weapons = [Weapon('top'), Weapon('bottom'), Weapon('front'), Weapon('back')]
+        weapons = [Weapon('left'), Weapon('right'), Weapon('front'), Weapon('back')]
 
     # counter clockwise
     def turn_left(self):
