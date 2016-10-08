ad1c1fb23fc4ed9b2263d2e3d37402578dc48a49
Billy Brown <wb33@st-andrews.ac.uk>
2016-10-08 17:24:51 +0100

    Improved stars

--- js/stars.js
+++ js/stars.js
@@ -8,8 +8,8 @@ class Stars {
 
   generate(width, height) {
     let stars = [];
-    const scale = 0.5;
-    const threshold = 0.7;
+    const scale = 0.1;
+    const threshold = 0.9;
     // Set the seed
     noise.seed(this.seed);
     for (let x = 0; x < width; ++x) {
@@ -17,12 +17,12 @@ class Stars {
         const posX = (x + this.offX) * scale;
         const posY = (y + this.offY) * scale;
         // Some pixels have stars on
-        if (noise.perlin2(posX, posY) > threshold) {
+        if (noise.simplex2(posX, posY) > threshold) {
           stars.push({
             x: x,
             y: y,
             // Stars have a random size
-            size: noise.simplex2(posX, posY) + 1
+            size: Math.abs(noise.perlin2(posX, posY)) * 4
           });
         }
       }
