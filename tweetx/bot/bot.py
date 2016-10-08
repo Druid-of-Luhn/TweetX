948654314788e43043ad85bdddccc114e577c70a
Kieran Kelk <kak3@st-andrews.ac.uk>
2016-10-08 18:23:22 +0100

    _listener, not listener.

--- tweetx/bot/bot.py
+++ tweetx/bot/bot.py
@@ -40,7 +40,7 @@ class TwitterBot():
         self._api.update_status('TweetX went online @ {}'.format(self._current_time()))
 
         self._listener = ReplyListener(self._voter, self._api)
-        self._stream = tweepy.Stream(auth = self._api.auth, listener=listener)
+        self._stream = tweepy.Stream(auth = self._api.auth, listener=self._listener)
         self._stream.filter(track=['@{}'.format(BOT_NAME)], async=True)
 
     def stop(self, crashed=False):
