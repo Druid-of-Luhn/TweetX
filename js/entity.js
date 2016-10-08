da394555ef43def3420cec305af29e0cb1eac774
Billy Brown <wb33@st-andrews.ac.uk>
2016-10-08 20:14:26 +0100

    Sprite dimensions

--- js/entity.js
+++ js/entity.js
@@ -1,5 +1,6 @@
 class Entity {
   static draw(render, entity) {
+    render.fillStyle = 'white';
     render.save();
     // Rotate the sprite
     if (entity.hasOwnProperty('direction')) {
@@ -9,17 +10,22 @@ class Entity {
     if (entity.type === 'Spaceship') {
       // Centre the player
       const pos = norm(0, 0);
-      render.drawImage(document.images[0], pos.x, pos.y);
+      render.drawImage(document.images[0], pos.x, pos.y, entity.width, entity.height);
     } else {
       // Place the entity
       const pos = norm(entity.pos[0], entity.pos[1]);
-      // Draw a dolphin
       if (entity.type === 'Dolphin') {
-        render.drawImage(document.images[1], pos.x, pos.y);
+        // Draw a dolphin
+        render.drawImage(document.images[1], pos.x, pos.y, entity.width, entity.height);
+      } else if (entity.type === 'Planet') {
+        // Draw a planet
+        render.beginPath();
+        render.arc(pos.x, pos.y, entity.width, 0, 2 * Math.PI, true);
+        render.closePath();
+        render.fill();
       } else {
         // Draw an unknown entity
-        render.fillStyle = 'white';
-        render.fillRect(pos.x, pos.y, 100, 80);
+        render.fillRect(pos.x, pos.y, entity.width, entity.height);
       }
     }
     render.restore();
