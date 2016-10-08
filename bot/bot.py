a9617c4c691de0e90332b116ba4d598ba11f4856
Kieran Kelk <kak3@st-andrews.ac.uk>
2016-10-08 15:38:44 +0100

    Parsing WIP.

--- bot/bot.py
+++ bot/bot.py
@@ -1,33 +1,14 @@
 import tweepy
-from enum import Enum
 from vote import VoteCounter
 
 from secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 
-class Command(Enum):
-    TURN_LEFT = 1
-    TURN_RIGHT = 2
-    FORWARD = 3
-
 voter = VoteCounter()
 
 class ReplyListener(tweepy.StreamListener):
-    def _handle_command(self, text):
-        if 'TURN_LEFT' in text:
-            voter.vote(Command.TURN_LEFT)
-            return True
-        elif 'TURN_RIGHT' in text:
-            voter.vote(Command.TURN_RIGHT)
-            return True
-        elif 'FORWARD' in text:
-            voter.vote(Command.FORWARD)
-            return True
-
-        return False
-
     def on_status(self, status):
         # TODO Stick these on a queue in future, so we don't fall behind.
-        valid = self._handle_command(status.text)
+        valid = voter.vote(status)
 
         if not valid:
             api.update_status("@{}: Sorry, it looks like that wasn't a valid command.".format(status.user.screen_name), in_reply_to_status_id=status.id)
@@ -41,11 +22,9 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status("We're online!")
-
 try:
     listener = ReplyListener()
     stream = tweepy.Stream(auth = api.auth, listener=listener)
     stream.filter(track=['@TweetX_Bot'])
 except KeyboardInterrupt:
-    api.update_status("We're down.")
+    pass
--- bot/vote.py
+++ bot/vote.py
@@ -1,5 +1,27 @@
 import operator
+
+from enum import Enum
+from fuzzywuzzy import process
 from collections import defaultdict
+
+REQUIRED_CERTAINTY = 60
+
+class Command(Enum):
+    FORWARD = 1
+    LEFT = 2
+    RIGHT = 3
+
+CHOICES = {
+    'forward': Command.FORWARD,
+    'straight': Command.FORWARD,
+    'onwards': Command.FORWARD,
+
+    'left': Command.LEFT,
+    'port': Command.LEFT,
+
+    'right': Command.RIGHT,
+    'starboard': Command.RIGHT
+}
 
 class VoteCounter(object):
     def __init__(self):
@@ -11,6 +33,17 @@ class VoteCounter(object):
             self._votes = defaultdict(int)
             return highest
 
-    def vote(self, move):
+    def vote(self, string):
+        move = self._parse_command(string)
         print(move)
-        self._votes[move] += 1
+        if move:
+            self._votes[move] += 1
+            return True
+
+        return False
+
+    def _parse_command(self, string):
+        result, certainty = process.extractOne(string, CHOICES.keys())
+
+        if certainty > REQUIRED_CERTAINTY:
+            return CHOICES[result]
