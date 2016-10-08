f8e467dd030434263bb9f8a211e7a571a8f4a894
Billy Brown <wb33@st-andrews.ac.uk>
2016-10-08 18:49:35 +0100

    Added sprites

--- js/entity.js
+++ js/entity.js
@@ -1,7 +1,20 @@
 class Entity {
   static draw(render, entity) {
-    render.fillStyle = 'white';
-    const pos = norm(entity.pos[0], entity.pos[1]);
-    render.fillRect(pos.x, pos.y, 150, 150);
+    if (entity.type === 'Spaceship') {
+      // Centre the player
+      const pos = norm(0, 0);
+      render.drawImage(document.images[0], pos.x, pos.y);
+    } else {
+      // Place the entity
+      const pos = norm(entity.pos[0], entity.pos[1]);
+      // Draw a dolphin
+      if (entity.type === 'Dolphin') {
+        render.drawImage(document.images[1], pos.x, pos.y);
+      } else {
+        // Draw an unknown entity
+        render.fillStyle = 'white';
+        render.fillRect(pos.x, pos.y, 100, 80);
+      }
+    }
   }
 }
